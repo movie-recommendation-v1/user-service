015393facb8f3658c0c6f680332026243c2e8e12
Mike <muhammmadazizekubov@gmail.com>
2024-10-13 18:02:41 +0500

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/movie-recommendation-v1/user-service
 
 go 1.22.1
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/spf13/cast v1.7.0
+	go.uber.org/zap v1.27.0
+	google.golang.org/grpc v1.67.1
+	google.golang.org/protobuf v1.35.1
+)
+
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
+)
---
+++ internal/logger/logrus.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+func NewLogger() (*zap.Logger, error) {
+	logFile, err := os.Create("app.logger")
+	if err != nil {
+		return nil, err
+	}
+
+	// Configure the encoder
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+	encoder := zapcore.NewJSONEncoder(encoderConfig)
+
+	// Create core with file write syncer
+	fileWriteSyncer := zapcore.AddSync(logFile)
+	consoleWriteSyncer := zapcore.AddSync(os.Stdout)
+	multiWriteSyncer := zapcore.NewMultiWriteSyncer(fileWriteSyncer, consoleWriteSyncer)
+	core := zapcore.NewCore(encoder, multiWriteSyncer, zapcore.DebugLevel)
+
+	// Create logger
+	logger := zap.New(core)
+	return logger, nil
+}
