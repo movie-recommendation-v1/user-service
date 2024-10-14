afdd1b078fce9021f323d2818e360fa0aca9b03b
Avazbek-02 <avazbekmambetov9@gmail.com>
2024-10-14 13:31:22 +0500

    done

--- internal/migration/000001_auth-service.up.sql
+++ internal/migration/000001_auth-service.up.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS users(
     id uuid primary key not null,
     name varchar(100) not null,
     lastname varchar(100) default 'empty',
-    email varchar(150) not null,
+    email varchar(150) unique not null,
     password varchar(350) not null,
-    role role not null,
+    role role default 'user' not null,
     created_at timestamp default now() not null,
     updated_at timestamp default now() not null,
     deleted_at bigint default 0 not null
