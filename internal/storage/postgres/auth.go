c7d6ce71360b5cff9e551f3bd2baab78d5913c4a
Avazbek-02 <avazbekmambetov9@gmail.com>
2024-11-14 18:27:45 +0500

    done

--- internal/storage/postgres/auth.go
+++ internal/storage/postgres/auth.go
@@ -361,6 +361,6 @@ func (s *UserRepo) UpdateUser(ctx context.Context, req *pb.UpdateUserReq) (*pb.U
 		return nil, err
 	}
 
-	// Return success response
+	// dskjhfgjksdfhgjkd
 	return &pb.UpdateUserRes{UserRes: req.UserReq}, nil
 }
