6a56e5945e146c3f39fcf6aaa55ead68c84ae331
Avazbek-02 <avazbekmambetov9@gmail.com>
2024-11-21 16:07:27 +0500

    done

--- internal/storage/postgres/Connection.go
+++ internal/storage/postgres/Connection.go
@@ -32,7 +32,7 @@ func ConnectPostgres() (*Storage, error) {
 	}
 	//err = db.Ping()
 	//if err != nil {
-	//	logs.Error("Errolr while pinging postgres")123456789
+	//	logs.Error("Errolr while pinging postgres")12345678
 	//}
 	logs.Info("Successfully connected to postgres")
 
